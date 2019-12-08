2b2bd3c7f29574e8a6e10b1a6dfb7a2eade8e218
Зайчик Руслан Игоревич <Zajc_od33>
2019-12-08 14:33:34 +0200

    F

---
+++ Function.js
@@ -0,0 +1,84 @@
+function showMessage() {
+    alert('Hello everybody')
+}
+showMessage();
+
+
+function sum (a,b) {
+    return  a + b;
+}
+var s=sum(2,3);
+alert("Sum:" + s);
+
+function div(firstNum,secondNum){
+    alert(firstNum + " * " +
+    secondNum + " * " +
+    (firstNum + secondNum));
+    return firstNum+secondNum
+}
+div(1,2);
+
+
+function Star(count) {
+    for (var i=0; i<count; i++) {
+        document.write(text'*');
+
+    }
+    document.write(text"<br>");
+}
+Star(count 30);
+Star(count 10);
+Star(count 50);
+
+function vivod(num) {
+    var n=parseInt(num);
+    if (n>0 || n<10) alert(n);
+    else alert("No coincidence");
+}
+vivod(15);
+
+function AnyLine(symb, count) {
+    for (var i=0; i<count; i++){
+        document.write(symb);
+    }
+    document.write(<br>);
+}
+AnyLine(symb'|', count 10);
+AnyLine(symb'++', count 3);
+AnyLine(symb'=', count 5);
+
+function MyPow(Digit,Pow) {
+    var key=1;
+    for (var i=0; i<pow; i++);
+    key *=Digit;
+return key;}
+
+
+function Change(One,Two) {
+    document.write("Enter in func" + One +Two+ "<br>");
+
+    var temp=One;
+    One=Two;
+    Two=temp;
+    document.write("Enter in func" + One +Two+ "<br>");
+}
+var a=1 , b=2
+document.write("ulululululululululululululu"+ One +Two+ "<br>")
+
+
+function go(a,b) {
+    alert("a=" + a +",b=" + b);
+}
+go(1);
+go(1,2);
+go(1,2,3);
+
+
+function sayHi() {
+    for(var i=0 ; i<arguments.length; i++){
+        alert("Hi"+ arguments[i]);
+    }
+}
+sayHi('Winni','Petachok','Erik','')]
+
+
